2ba8efd23ce77b3e66ef1bbb47e9ac40cc55e256
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2024-11-18 17:47:40 +0100

    Home Property Listings

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
+import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
-import Link from "next/link";
+
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };

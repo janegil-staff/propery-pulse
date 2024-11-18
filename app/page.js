e74750e30131a8d1e3d1be88ac295eb2ab145cc3
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2024-11-18 15:57:53 +0100

    Homepage Component & Footer

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 import Link from "next/link";
 
 const HomePage = () => {
   return (
     <>
-      <h1 className="text-3xl">Welcome</h1>
-      <Link href="/properties">Go to properties</Link>
+      <Hero />
+      <InfoBoxes />
     </>
   );
 };
